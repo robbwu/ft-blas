89f461465614c7613e80a21e072a3dfa711ee051
Robb Wu <armiuswu@gmail.com>
2011-06-12 10:09:23 +0800

    final version for submitting.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 test: ft_blas.c Makefile
 		#cc -Wall -framework Accelerate -lgsl ft_blas.c -o test
 		cc  -framework Accelerate -lgsl ft_blas.c -o test -O2
-		./test
+		#./test
 
 clean:
 		rm test stock *.o
--- ft_blas.c
+++ ft_blas.c
@@ -1,5 +1,11 @@
+/* USE AT YOUR OWN RISKS!!
+ * Author: Panruo Wu(armiusuw@gmail.com)
+ * Last Modified Date: June 9 2011
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <math.h>
 #include <time.h>
 #include <gsl/gsl_rng.h>
@@ -11,10 +17,14 @@
 #ifndef GSL_RANGE_CHECK_OFF
 #define GSL_RANGE_CHECK_OFF
 #endif
-#define N 500
+/*#define N 2000*/
+/*#define ER 1*/
+long int N, ER;
+
 double *row_sums, *col_sums, all_sum;
 double u = 2.2e-16;
 double lambda, mu, tau;  // thresholds for distinguishing roundoff between fault
+gsl_matrix *target;
 
 double infnorm(gsl_matrix *A)
 {
@@ -67,7 +77,7 @@ double vec_sum(gsl_vector *v)
     
     return sum;
 }
-void build_checksum(gsl_matrix *A, gsl_matrix *B)
+void build_checksum(gsl_matrix *A, gsl_matrix *B, gsl_matrix *C)
 {
     gsl_vector_view sum, v;
     int i;
@@ -87,6 +97,8 @@ void build_checksum(gsl_matrix *A, gsl_matrix *B)
         v = gsl_matrix_column(B, i);
         gsl_vector_add(&sum.vector, &v.vector);
     }
+
+    gsl_matrix_set_zero(C);
 
 }
 int ft_dgemm(gsl_matrix *A, gsl_matrix *B, gsl_matrix *C, int rank)
@@ -109,14 +121,13 @@ int ft_dgemm(gsl_matrix *A, gsl_matrix *B, gsl_matrix *C, int rank)
     // build checksum matrices A^c, B^r
 retry:
     trials++;
-    build_checksum(A, B);
+    build_checksum(A, B, C);
 
     
     /*printf("[ft_dgemm]: (m, k, n) = (%d, %d, %d)\n", m, k, n);*/
     for (s = 0; s < k/rank * rank; s+=rank) {
-        gsl_matrix_set(C, 0, 0, 2010.0);
         err = verify_checksum(C);
-        /*printf("err: %d", err); */
+        /*printf("err: %d\n", err); */
         if (err < 0 && trials < 5)
             goto retry;
         Av = gsl_matrix_submatrix(A, 0, s, mm, rank);
@@ -126,6 +137,7 @@ retry:
 
     if (s < k) {
         err = verify_checksum(C);
+        /*printf("err: %d\n", err); */
         if (err < 0 && trials < 5)
             goto retry;
         Av = gsl_matrix_submatrix(A, 0, s, mm, k-s);
@@ -160,6 +172,10 @@ int verify_checksum(gsl_matrix *C)
     double c;
 
     // calculate row & col sums.
+
+    memset(row_sums, 0, sizeof(double)*(m-1));
+    memset(col_sums, 0, sizeof(double)*(n-1));
+
     for (i = 0; i < m-1; i++) {
         for (j = 0; j < n-1; j++) {
             c = gsl_matrix_get(C, i, j);
@@ -189,7 +205,9 @@ int verify_checksum(gsl_matrix *C)
         if ( fabs(row_sums[ii] - col_sums[jj]) > tau )
             return -1;
         else {
-            gsl_matrix_set( C, ii, jj, -row_sums[ii] + gsl_matrix_get(C, i, j));
+            /*printf("row_sums[ii] = %f, C[ii, jj] = %f\n" , */
+                    /*row_sums[ii], gsl_matrix_get(C, ii, jj));*/
+            gsl_matrix_set( C, ii, jj, -row_sums[ii] + gsl_matrix_get(C, ii, jj));
             return 1;
         }
     } else if (in == 0  && jn == 0) 
@@ -198,42 +216,98 @@ int verify_checksum(gsl_matrix *C)
         return -2;
 }
 
+void* noise_thread(void *vargp)
+{
+    gsl_rng * r = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(r, clock());
+    int i, j;
+    double mag;
+    long int msecs;
+
+    pthread_setcanceltype(PTHREAD_CANCEL_ENABLE, NULL);
+
+    while(1) { 
+        msecs = floor(0.8 * N/1000.0 * N * N / ER * 
+                (1+fabs(gsl_ran_gaussian_ziggurat(r, 1))));
+        /*msecs = floor(0.8 * N/1000. * N * N / ER);*/
+        /*printf("secs: %f\n", msecs/1.e6);*/
+        usleep(msecs);
+        i = floor(gsl_rng_uniform(r) * (N-1));
+        j = floor(gsl_rng_uniform(r) * (N-1));
+        mag = gsl_ran_gaussian_ziggurat(r, 10);
+        /*printf("C[%d, %d] = %f  ", i, j, gsl_matrix_get(target, i, j));*/
+        gsl_matrix_set(target, i, j, gsl_matrix_get(target, i, j)* mag);
+        /*printf("noise: C[%d, %d] = %.3f\n", i, j, mag*gsl_matrix_get(target, i, j)); */
+    }
+
+    gsl_rng_free(r);
+    return NULL;
+}
 void test_ft_dgemm()
 {
 
     float t0, t1;
-    int m, n, k;
-    gsl_matrix *A, *B, *C;
+    int m, n, k, s;
+    gsl_matrix *A, *B, *C, *D;
     A = gsl_matrix_alloc(N, N);
     B = gsl_matrix_alloc(N, N);
     C = gsl_matrix_alloc(N, N);
+    D = gsl_matrix_alloc(N, N);
+    target = C;
     m = A->size1; n = B->size2; k = A->size2;
     randomize_matrix(A);
     randomize_matrix(B);
     row_sums = calloc(C->size1, sizeof(double));
     col_sums = calloc(C->size2, sizeof(double));
 
-    build_checksum(A, B);
+    build_checksum(A, B, C);
     lambda = infnorm(A) * infnorm(B) * k * u;
     mu = onenorm(A) * onenorm(B) * k * u;
     tau = (lambda < mu) ? lambda : mu;
 
+    pthread_t tid;
+    pthread_create(&tid, NULL, noise_thread, NULL);
+
+
+    /*s = floor(MET * 1.0e8 /N/N);*/
+    s = floor(N/2./ER);
+    s = (s < 100) ? 100 : s;
+    /*printf("%d\n", s);*/
     t0 = clock();
-    ft_dgemm(A, B, C, 200);
+    ft_dgemm(A, B, C, 100);
     t1 = clock();
-    printf("ft_dgemm: %.3f\n", 2* pow((N/1000.), 3) * CLOCKS_PER_SEC / (t1-t0));
+    printf("ft_dgemm: %.3fGFLOPS %.3fs\n", 
+            2* pow((N/1000.), 3) * CLOCKS_PER_SEC / (t1-t0), (t1-t0)/CLOCKS_PER_SEC);
+    pthread_cancel(tid);
 
     t0 = clock();
-    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A, B, 0.0, C);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A, B, 0.0, D);
     t1 = clock();
-    printf("dgemm: %.3f\n", 2* pow((N/1000.), 3) * CLOCKS_PER_SEC / (t1-t0));
+    printf("dgemm: %.3fGFLPS %.3fs\n", 
+            2* pow((N/1000.), 3) * CLOCKS_PER_SEC / (t1-t0), (t1-t0)/CLOCKS_PER_SEC);
+    gsl_matrix_sub(C,D); 
+    printf("error(infnorm): %f\n", infnorm(C));
+
+    pthread_create(&tid, NULL, noise_thread, NULL);
+    t0 = clock();
+    ft_dgemm(A, B, C, N);
+    t1 = clock();
+    printf("huang_dgemm: %.3fGFLOPS %.3f\n", 
+            2* pow((N/1000.), 3) * CLOCKS_PER_SEC / (t1-t0), (t1-t0)/CLOCKS_PER_SEC);
+    pthread_cancel(tid);
+    gsl_matrix_sub(C,D); 
+    printf("error(infnorm): %f\n", infnorm(C));
+
 
     gsl_matrix_free(A);
     gsl_matrix_free(B);
     gsl_matrix_free(C);
+    gsl_matrix_free(D);
     free(row_sums);free(col_sums);
 }
 int main(int argc, char *argv[])
 {
+    N = atoi(argv[1]);
+    ER = atoi(argv[2]);
     test_ft_dgemm();
 }
